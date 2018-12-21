e6612f0cbdc6a7d7e23ea32dd27855a3ec54c542
Vladimir Pak <theratg@gmail.com>
2018-12-21 08:43:42 +0200

    added dep

---
+++ Command/LinodeInstanceCloneFromBackupCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace TheRat\LinodeBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class LinodeInstanceCloneFromBackupCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('linode:instances:clone-from-backup')
+            ->addArgument('linode-id', InputArgument::REQUIRED, 'ID of the Linode to look up')
+            ->setDescription('Clone linode instance from backup');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+        $io->title('Clone linode instance from backup');
+    }
+}
--- Services/LinodeInstancesService.php
+++ Services/LinodeInstancesService.php
@@ -5,12 +5,40 @@ namespace TheRat\LinodeBundle\Services;
 use GuzzleHttp\Psr7\ServerRequest;
 use TheRat\LinodeBundle\Aware\LinodeClientAwareInterface;
 use TheRat\LinodeBundle\Aware\LinodeClientAwareTrait;
+use TheRat\LinodeBundle\Response\ItemResponse;
+use TheRat\LinodeBundle\Response\ListResponse;
 
 class LinodeInstancesService implements LinodeClientAwareInterface
 {
     use LinodeClientAwareTrait;
 
-    public function loadList(int $page = 1, int $pageSize = 100)
+    public function view(int $linodeId)
+    {
+        $request = new ServerRequest('GET', 'linode/instances/' . $linodeId);
+        $result = $this->getLinodeClient()->send($request);
+
+        return $result;
+    }
+
+    public function viewByLabel($label): ?ItemResponse
+    {
+        $result = null;
+        $page = 1;
+        do {
+            $response = $this->loadList($page++);
+            $rows = $response->getData();
+            foreach ($rows as $row) {
+                if ($label === $row['label']) {
+                    $result = $row;
+                    break;
+                }
+            }
+        } while (!empty($rows));
+
+        return $result;
+    }
+
+    public function loadList(int $page = 1, int $pageSize = 100): ?ListResponse
     {
         $request = new ServerRequest('GET', 'linode/instances');
         $request->withQueryParams([
@@ -21,13 +49,4 @@ class LinodeInstancesService implements LinodeClientAwareInterface
 
         return $result;
     }
-
-    public function view(int $linodeId)
-    {
-        $request = new ServerRequest('GET', 'linode/instances/' . $linodeId);
-        $result = $this->getLinodeClient()->send($request);
-
-        return $result;
-    }
-
 }
